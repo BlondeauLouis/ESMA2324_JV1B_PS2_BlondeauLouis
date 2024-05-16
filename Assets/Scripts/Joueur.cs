cdf7f91571321529c5d2ce4b0df6c68c81498248
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-05-16 10:25:27 +0200

    Jauge Métagmorphose

--- Assets/Scripts/Joueur.cs
+++ Assets/Scripts/Joueur.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float jump;
     public float health;
+
+    public Image invisibilityGauge;
+    public float maxInvisibilityDuration = 5f;
+    public float currentInvisibilityDuration;
+    public float invisibilityRechargeRate = 1f;
 
     private Rigidbody2D rb;
     private SpriteRenderer SpriteRenderer;
@@ -20,14 +26,13 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
+        currentInvisibilityDuration = maxInvisibilityDuration;
     }
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-
         Vector3 direction = new Vector2(moveHorizontal, 0);
-
         transform.Translate(direction * speed * Time.deltaTime);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
@@ -35,13 +40,29 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jump);
         }
 
+        currentInvisibilityDuration = Mathf.Clamp(currentInvisibilityDuration, 0, maxInvisibilityDuration);
+
         if (isInvisible)
         {
             SpriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
+            currentInvisibilityDuration -= Time.deltaTime;
+            if (currentInvisibilityDuration <= 0)
+            {
+                isInvisible = false;
+            }
         }
         else
         {
             SpriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            if (currentInvisibilityDuration < maxInvisibilityDuration)
+            {
+                currentInvisibilityDuration += invisibilityRechargeRate * Time.deltaTime;
+            }
+        }
+
+        if (invisibilityGauge != null)
+        {
+            invisibilityGauge.fillAmount = currentInvisibilityDuration / maxInvisibilityDuration;
         }
     }
 
