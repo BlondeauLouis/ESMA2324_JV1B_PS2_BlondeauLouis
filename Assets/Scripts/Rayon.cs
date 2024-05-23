bf068a23d9422f22e4a97812d4f98238306c7671
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-05-23 10:29:53 +0200

    Esquiver la cam

--- Assets/Scripts/Rayon.cs
+++ Assets/Scripts/Rayon.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class Ray : MonoBehaviour
 {
     public GameObject pont;
+    private PlayerController playerController;
+    private Collider2D enemyCollider;
     // Start is called before the first frame update
     void Start()
     {
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
 
+        enemyCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerController.isInvisible)
+        {
+            Physics2D.IgnoreCollision(playerController.GetComponent<Collider2D>(), enemyCollider, true);
+        }
+        else
+        {
+            Physics2D.IgnoreCollision(playerController.GetComponent<Collider2D>(), enemyCollider, false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
