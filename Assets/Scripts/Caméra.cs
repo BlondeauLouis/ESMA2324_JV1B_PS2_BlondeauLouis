fc685c31deabdda95f82e945b87c655a288a4217
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-05-23 09:39:41 +0200

    Caméra et pont

---
+++ Assets/Scripts/Caméra.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject pont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            pont.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            pont.SetActive(true);
+        }
+    }
+}
