aba4aec7063446d859e861866a8c82ff402eeb42
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-05-23 11:12:09 +0200

    Si joueur invisible, ennemi ne bouge plus

--- Assets/Scripts/FlicCam.cs
+++ Assets/Scripts/FlicCam.cs
@@ -8,6 +8,7 @@ public class FlicCam : MonoBehaviour
     private PlayerController playerController;
 
     private Transform playerTransform;
+    private float initialY;
 
     void Start()
     {
@@ -15,30 +16,37 @@ public class FlicCam : MonoBehaviour
         enemyCollider = GetComponent<Collider2D>();
         playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        initialY = transform.position.y; // Sauvegarder la position initiale en Y
     }
 
     void Update()
     {
-
         // Ignorer les collisions avec le joueur si le joueur est invisible
         if (playerController.isInvisible)
         {
             Physics2D.IgnoreCollision(playerController.GetComponent<Collider2D>(), enemyCollider, true);
+            rgbd.velocity = Vector2.zero; // Arrêter le mouvement si le joueur est invisible
         }
         else
         {
             Physics2D.IgnoreCollision(playerController.GetComponent<Collider2D>(), enemyCollider, false);
+
+            if (playerTransform != null)
+            {
+                Vector2 direction = (playerTransform.position - transform.position).normalized;
+                direction.y = 0; // Contraindre le mouvement uniquement sur l'axe X
+                rgbd.velocity = direction * speed;
+            }
+            else
+            {
+                Debug.Log("Player not found");
+            }
         }
 
-        if (playerTransform != null)
-        {
-            Vector3 direction = (playerTransform.position - transform.position).normalized;
-            transform.position += direction * speed * Time.deltaTime;
-        }
-        else
-        {
-            Debug.Log("samarchpas");
-        }
+        // Verrouiller la position en Y pour éviter l'enfoncement
+        Vector3 position = transform.position;
+        position.y = initialY;
+        transform.position = position;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
