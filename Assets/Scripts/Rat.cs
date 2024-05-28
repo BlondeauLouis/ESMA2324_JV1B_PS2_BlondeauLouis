a14695c33291673a157ccf9c53f113b8b387887e
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-05-28 12:28:19 +0200

    Rat (attention bug)

---
+++ Assets/Scripts/Rat.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rat : MonoBehaviour
+{
+    public bool toleft = true;
+    public float speed;
+    private Rigidbody2D rgbd;
+    private Collider2D enemyCollider;
+    private PlayerController playerController;
+
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+        enemyCollider = GetComponent<Collider2D>();
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>(); // Obtenez la référence au script du joueur
+    }
+
+    void Update()
+    {
+        if (toleft)
+        {
+            rgbd.velocity = new Vector2(-speed, rgbd.velocity.y);
+        }
+        else
+        {
+            rgbd.velocity = new Vector2(speed, rgbd.velocity.y);
+        }
+
+        // Ignorer les collisions avec le joueur si le joueur est invisible
+        if (playerController.isInvisible)
+        {
+            Physics2D.IgnoreCollision(playerController.GetComponent<Collider2D>(), enemyCollider, true);
+        }
+        else
+        {
+            Physics2D.IgnoreCollision(playerController.GetComponent<Collider2D>(), enemyCollider, false);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("LeftLimit"))
+        {
+            toleft = false;
+        }
+
+        if (other.CompareTag("RightLimit"))
+        {
+            toleft = true;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            playerController.PerdPv();
+        }
+    }
+}
