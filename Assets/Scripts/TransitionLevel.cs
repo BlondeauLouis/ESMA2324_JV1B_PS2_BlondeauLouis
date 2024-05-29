18b8ebd647f562625d802f35b040b1b2a9dabc6a
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-05-29 15:37:37 +0200

    Fondu au noir

--- Assets/Scripts/TransitionLevel.cs
+++ Assets/Scripts/TransitionLevel.cs
@@ -1,18 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class TransitionLevel : MonoBehaviour
+public class SceneTransition : MonoBehaviour
 {
-    public string nextSceneName;
-    public float transitionDelay = 3f;
+    public string nextSceneName; // Le nom de la prochaine scène à charger
+    public float transitionDelay = 3f; // Délai en secondes avant de passer à la prochaine scène
+
+    private Renderer transitionRenderer;
+    private Color originalColor;
 
     void Start()
     {
-        Invoke("LoadNextScene", transitionDelay);
+        transitionRenderer = GetComponentInChildren<Renderer>();
+        originalColor = transitionRenderer.material.color;
+
+        // Démarrez la coroutine de fondu au noir
+        StartCoroutine(FadeToBlack());
     }
 
-    void LoadNextScene()
+    IEnumerator FadeToBlack()
     {
+        float t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / transitionDelay;
+            Color newColor = Color.Lerp(originalColor, Color.black, t);
+            newColor.a = Mathf.Clamp01(newColor.a); // Assure que l'alpha reste entre 0 et 1
+            transitionRenderer.material.color = newColor;
+            yield return null;
+        }
+
+        // Charger la prochaine scène
         SceneManager.LoadScene(nextSceneName);
     }
 }
