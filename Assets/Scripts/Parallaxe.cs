268e46750f756776af8971223627a7369d96d70b
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-06-05 09:44:40 +0200

    Parallaxe de Julien <3

--- Assets/Scripts/Parallaxe.cs
+++ Assets/Scripts/Parallaxe.cs
@@ -1,24 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
 
-public class Parallax : MonoBehaviour
+public class Parallaxe : MonoBehaviour
 {
-    public float parallaxSpeed = 0.5f; // Vitesse de défilement du parallaxe
-    private float startPosX;
-    private float lengthX;
-    private CinemachineVirtualCamera virtualCamera;
-
+    public Transform target;
+    public float smoothing;
+    public int level; //niveau d'arrière plan
+    private float initialPositionx;
+    private float initialPositiony;
+    // Start is called before the first frame update
     void Start()
     {
-        startPosX = transform.position.x;
-        lengthX = GetComponent<SpriteRenderer>().bounds.size.x;
-        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        initialPositionx = gameObject.transform.position.x; //récupère la position initiale en x
+        initialPositiony = gameObject.transform.position.y; //récupère la position initiale en y
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        float dist = (virtualCamera.transform.position.x * parallaxSpeed);
-
-        transform.position = new Vector3(startPosX + dist, transform.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(target.position.x + (initialPositionx*level), initialPositiony*level, 5) / level; //calcule la position visée
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing); //effectue le mouvement
     }
 }
+
+//Merci Julien !!!!
